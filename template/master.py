0d887658dacb0b3b195e25d142662df6c459842e
sifi_border <shusaku5296@gmail.com>
2018-10-21 22:53:44 +0900

    first commit

---
+++ template/master.py
@@ -0,0 +1,19 @@
+import urllib.request
+
+protain_list = []
+
+in_f = open("rosalind_xxxx.txt", 'r')
+out_f = open("output.txt", 'w')
+
+text = in_f.read()
+protain_names = text.splitlines()
+
+url_str = "https://www.uniprot.org/uniprot/{}.fasta".format(name)
+req = urllib.request.Request(url_str)
+with urllib.request.urlopen(req) as response:
+	raw_page = response.read()
+	raw_page = raw_page.decode('utf-8')
+	lines = raw_page.splitlines()
+
+in_f.close()
+out_f.close()
