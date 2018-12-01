2b3ea0d8022c6cf16ca348e7ca55ebf5e854947e
sifi_border <shusaku5296@gmail.com>
2018-12-02 01:03:30 +0900

    work and I/O

---
+++ workspace/tabaityo.hpp
@@ -0,0 +1,8 @@
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace std;
+
+namespace mp = boost::multiprecision;
+
+typedef mp::cpp_int infint;
+typedef vector <infint> vecinf;
+typedef vector <vecinf> matinf;
